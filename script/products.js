2354de16f38631f050bcfa8d4419a3b2ba21a918
Taflnne15 <taflynnepetersen@gmail.com>
2023-06-12 16:37:35 +0200

    done

--- script/products.js
+++ script/products.js
@@ -11,14 +11,40 @@ let products = [
     },{
         id: 3,
         products_image:'https://i.postimg.cc/76zhCpCJ/Ag304-Gibson-l-10.webp',
-        products_title: 'Gibson Acoustic L-00',
-        products_text:'Gibson was a pioneer in the development of small body guitars in the beginning of the 1900s with its "L-Series Small Body" acoustics. These smaller Gibsons deliver a tonality that is best described in Gibsons 1934 catalog as a sound of "perfect balance." ',
+        products_title: '1932 Gibson L-10 Acoustic',
+        products_text:'The relatively fancy L-10 seen here was made in early 1932 and is currently used by San Francisco guitarist Eddie Scher. Its fretboard is decorated with ornate rectangular picture-frame inlays—a motif borrowed from Gibsons highly ornamented banjos—and its headstock sports a double-handled vase inlay.',
     },{
         id: 4,
-        products_image:'',
-        products_title: 'Gibson Acoustic L-00',
-        products_text:'',
-    },
+        products_image:'https://i.postimg.cc/BbVzwzhC/flying-v.jpg',
+        products_title: 'Gibson 67 flying V Maestro GH',
+        products_text:'Gibson Customs 1967 Mahogany Flying V Reissue is a classic V, through and through. Youll love the rich midrange character and sustain you get from its mahogany body. The set mahogany neck fills your hand like a 60s Gibson, thanks to its "Authentic 67 Medium C" profile.',
+    },{id: 5,
+        products_image:'https://i.postimg.cc/pXFd2MDz/lespual-studio.webp',
+        products_title: 'Gibson Les Paul Studio',
+        products_text:'The original Les Paul Studio was redesigned in 1990 to include an ebony fretboard with trapezoid inlays in the same style as higher end Les Pauls, and 490R/498T Humbuckers pickups.',
+    },{
+        id: 6,
+        products_image:'https://i.postimg.cc/fTqmtNFL/sglespaul.jpg',
+        products_title: 'Gibson SG standard',
+        products_text:'The Gibson SG Standard rocks the classic looks and features associated with the late 60s style SG models sought after by many. A rounded profile mahogany neck, bound rosewood fingerboard, long tenon 19th fret neck joint, and a solid mahogany body provide the backbone for singing sustain .Truly one of Maniac Guitars favourites',
+    },{
+        id: 7,
+        products_image:'https://i.postimg.cc/cJsQvGGk/electric-guitars-cort-x-2-vn-fr.jpg',
+        products_title: 'Cort X 2 ',
+        products_text:'The sleek and thoroughly modern X Series guitars are built for speed, power and aggression. The extreme contours on the body and the neck joint provide comfort as well as a distinctively high-tech look. The super fast neck and large jumbo frets facilitate speed and precision in your playing.This guitar is built for speed.',
+     
+    },{
+        id: 8,
+        products_image:'https://i.postimg.cc/vTFPS8K8/Cort-G250.webp',
+        products_title: 'Cort G250 Black',
+        products_text:'The G Series represents a logical evolution of the term "vintage". It is modern yet retains the familiar characteristics of the classic double-cutaway design in an aesthetically tasteful manner. This double-cutaway collection includes a wide variety of options and features to best meet a players specific needs and requirements.',
+    },{
+        id: 9,
+        products_image:'https://i.postimg.cc/vTTNV2Cf/cort-cort-g250-spectrum-metallic-purple-electric-g.webp',
+        products_title: 'Cort G250 Spectrum pink',
+        products_text: 'G250 Spectrum is designed for guitarists that seek a simple, slick-looking guitar with retro styling and colors that pop, while applying essential advanced components to make the guitar both greatly playable and enjoyable. Colorful sparkling metallic finishes with matching headstock bring back nostalgia of the modern-retro era.',
+    }
+
 
 ]
 
@@ -30,11 +56,16 @@ products.forEach((reveal) =>{
     
   <div class="col">
     <class="card h-100">
-      <img src="${reveal.products_image}" class="card-img-top" alt="...">
+      <img src="${reveal.products_image}" class="card-img-top" alt="..." style="object-fit:contain;
+      aspect-ratio: 3/2;";>
       <div class="card-body">
         <h5 class="card-title">${reveal.products_title}</h5>
         <p class="card-text">${reveal.products_text}</p>
+        <button type="button" class="btn btn-primary btn-sm">Small button</button>
+
         
       </div>
     `
 })
+
+ 
